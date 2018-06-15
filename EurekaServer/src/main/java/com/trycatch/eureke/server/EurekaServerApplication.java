87fec9bf15001524fdcfbe4e6c40908c40bb65ad
root <1132576362@qq.com>
2018-06-15 10:37:30 +0800

    安全验证

--- EurekaServer/src/main/java/com/trycatch/eureke/server/EurekaServerApplication.java
+++ EurekaServer/src/main/java/com/trycatch/eureke/server/EurekaServerApplication.java
@@ -1,6 +1,6 @@
 package com.trycatch.eureke.server;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -12,10 +12,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application
+public class EurekaServerApplication
 {
     public static void main( String[] args )
     {
-       new SpringApplicationBuilder(Application.class).web(true).run(args);
+       new SpringApplicationBuilder(EurekaServerApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
--- EurekaServiceClient/src/main/java/com/trycatch/service/client/EurekaServiceClientApplication.java
+++ EurekaServiceClient/src/main/java/com/trycatch/service/client/EurekaServiceClientApplication.java
@@ -1,10 +1,10 @@
 package com.trycatch.service.client;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
@@ -12,11 +12,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @EntityScan("com.trycatch.eurka.service.client")
 @SpringBootApplication
-@EnableEurekaClient
-public class Application
+@EnableDiscoveryClient
+public class EurekaServiceClientApplication
 {
     public static void main( String[] args )
     {
-        new SpringApplicationBuilder(Application.class).web(true).run(args);
+         new SpringApplicationBuilder(EurekaServiceClientApplication.class).web(WebApplicationType.SERVLET).run(args);
     }
 }
